9feec35fb1e56eb109d762cdc1d0cea7843de4d3
siva_ganugula <98007068+21A91A04D0@users.noreply.github.com>
2024-01-25 23:49:06 +0530

    Time: 87 ms (10.45%) | Memory: 257.6 MB (5.18%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int fun(string &text1, string &text2, int i, int j, vector<vector<int>> &dp){
+    int fun(string &text1, string & text2, int i, int j, vector<vector<int>> &dp){
         if(i >= text1.size() || j >= text2.size()) return 0;
         if(dp[i][j] != -1) return dp[i][j];
         int ans = 0;
         int res = 0;
-        if(text1[i] == text2[j]) ans = 1 + fun(text1, text2, i+1, j+1, dp);
-        res = max(fun(text1, text2, i + 1, j, dp) , fun(text1, text2, i, j + 1, dp));
-        res = max(res,ans);
-        dp[i][j] = res;
-        return res;
+        if(text1[i] == text2[j]) res = 1 + fun(text1, text2, i+1, j+1, dp);
+        else {
+            res = max(fun(text1, text2, i + 1, j, dp) , fun(text1, text2, i, j + 1, dp));
+        }
+    
+        
+        return dp[i][j] = res;;
     }
     int longestCommonSubsequence(string text1, string text2) {
         if(text1 == text2) return text1.size();
