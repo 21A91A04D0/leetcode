aa7eef6cec7a0687392a2c59bf5cf90b02bc2d9d
siva_ganugula <98007068+21A91A04D0@users.noreply.github.com>
2024-01-26 23:22:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (60.04%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/out-of-boundary-paths.cpp
@@ -3,8 +3,7 @@ public:
     int dp[51][51][51];
     const int mod = 1e9 + 7;
     int helper(int r , int c, int m, int n, int moves) {
-        // int sm = 0;
-        if((r < 0 || c < 0 || r >= m || c >= n)) return 1;
+        if((r < 0 || c < 0 || r == m || c == n)) return 1;
         if(moves == 0) return 0;
         if(dp[r][c][moves] != -1) return dp[r][c][moves];
         int a = helper(r , c - 1, m, n, moves - 1) % mod;
