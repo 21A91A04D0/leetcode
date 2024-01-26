cdf8495a0fe9b2d6b0ac0e4d30a76b26b3904cbf
siva_ganugula <98007068+21A91A04D0@users.noreply.github.com>
2024-01-26 23:20:52 +0530

    Time: 5 ms (77.51%) | Memory: 7.8 MB (57.43%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/out-of-boundary-paths.cpp
@@ -5,7 +5,7 @@ public:
     int helper(int r , int c, int m, int n, int moves) {
         // int sm = 0;
         if((r < 0 || c < 0 || r >= m || c >= n)) return 1;
-        if(moves <= 0) return 0;
+        if(moves == 0) return 0;
         if(dp[r][c][moves] != -1) return dp[r][c][moves];
         int a = helper(r , c - 1, m, n, moves - 1) % mod;
         int b = helper(r - 1, c, m, n, moves - 1) % mod;
