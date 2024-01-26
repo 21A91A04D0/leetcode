f16126ddb36cb75214a7f6b54fde1fa53f8eb579
siva_ganugula <98007068+21A91A04D0@users.noreply.github.com>
2024-01-26 23:20:23 +0530

    Time: 3 ms (89.56%) | Memory: 7.7 MB (59.44%) - LeetSync

---
+++ 576-out-of-boundary-paths/out-of-boundary-paths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dp[51][51][51];
+    const int mod = 1e9 + 7;
+    int helper(int r , int c, int m, int n, int moves) {
+        // int sm = 0;
+        if((r < 0 || c < 0 || r >= m || c >= n)) return 1;
+        if(moves <= 0) return 0;
+        if(dp[r][c][moves] != -1) return dp[r][c][moves];
+        int a = helper(r , c - 1, m, n, moves - 1) % mod;
+        int b = helper(r - 1, c, m, n, moves - 1) % mod;
+        int cs = helper(r + 1, c, m, n, moves - 1) % mod;
+        int d = helper(r, c + 1, m, n, moves - 1) % mod;
+        
+        return dp[r][c][moves] = (((a + b) % mod) + ((cs + d % mod) % mod)) % mod;
+    }
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        memset(dp, -1, sizeof(dp));
+        return helper(startRow, startColumn, m, n, maxMove);
+        
+    }
+};
