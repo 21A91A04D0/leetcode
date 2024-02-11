fbf326e5e32a262832cda6fd7462a6fa06d5e704
siva_ganugula <98007068+21A91A04D0@users.noreply.github.com>
2024-02-12 01:01:15 +0530

    Time: 167 ms (21.60%) | Memory: 33.2 MB (5.47%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int dp[71][71][71];
-    int grid(int i, int j, int i1, int j1, vector<vector<int>> &mat, int m, int n){
+    int grid(int i, int j, int j1, vector<vector<int>> &mat, int m, int n){
         if(j < 0 || j1 < 0 || j == n || j1 == n) return 0;
-        if(i == m and i1 == m) return 0;
+        if(i == m) return 0;
         if(dp[i][j][j1] != -1) return dp[i][j][j1];
         int res = 0;
         vector<int> a = {-1, 0, 1};
         for(auto q : a) {
             for(auto w : a) {
                 if(j == j1){
-                    res = max(res,mat[i][j] + grid(i + 1, j + q, i1 + 1, j1 + w, mat, m, n));
+                    res = max(res,mat[i][j] + grid(i + 1, j + q, j1 + w, mat, m, n));
                 }
                 else{
-                    res = max(res,mat[i][j] + mat[i1][j1] + grid(i + 1, j + q, i1 + 1, j1 + w, mat, m, n));
+                    res = max(res,mat[i][j] + mat[i][j1] + grid(i + 1, j + q, j1 + w, mat, m, n));
                 }
-                
             }
         }
         return dp[i][j][j1] = res;
@@ -24,6 +23,6 @@ public:
         int m = gride.size();
         int n = gride[0].size();
         memset(dp, -1, sizeof(dp));
-        return grid(0, 0, 0, n - 1, gride, m, n);
+        return grid(0, 0, n - 1, gride, m, n);
     }
 };
