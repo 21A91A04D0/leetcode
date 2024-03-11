ec70f0a00da7bbbc66daed096d93b16fd959cc7f
siva_ganugula <98007068+21A91A04D0@users.noreply.github.com>
2024-03-11 11:54:11 +0530

    Time: 1133 ms (27.24%) | Memory: 0B (100.00%) - LeetSync

--- 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
 # for each row with unique teacher_id (group by) with the count of distinct subject_id aliasing count as cnt from the Teacher table  
-SELECT teacher_id, count(distinct subject_id) as cnt from Teacher
+SELECT teacher_id, count(distinct subject_id) as cnt
+from Teacher
 group by teacher_id;
