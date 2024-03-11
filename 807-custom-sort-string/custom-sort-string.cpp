3d3ef4a221214795cb89de5af7604d52d352e0d3
siva_ganugula <98007068+21A91A04D0@users.noreply.github.com>
2024-03-11 06:54:59 +0530

    Time: 3 ms (50.27%) | Memory: 7.4 MB (78.20%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        string in = "";
-        string no = "";
-        map<char, int> o_mp;
-        map<char, int> s_mp;
-        for(auto it : order) {
-            o_mp[it]++;
-        }
-        for(auto it : s) {
-            s_mp[it]++;
-        }
+        string ans;
+        vector<int> freq(26, 0);
         for(int i = 0 ; i < s.size() ; i++) {
-            if(o_mp[s[i]] != 0) {
-                in.push_back(s[i]);
-            }
-            else {
-                no.push_back(s[i]);
-            }
+            freq[s[i] - 'a']++;
         }
-        map<char, int> a_mp;
-        for(auto it : in) {
-            a_mp[it]++;
-        }
-        string ans = "";
         for(int i = 0 ; i < order.size() ; i++) {
-            if(a_mp[order[i]] != 0) {
-                int f = a_mp[order[i]];
-                while(f--) {
-                    ans.push_back(order[i]);
-                }
-                
+            while(freq[order[i] - 'a']-- > 0) {
+                ans.push_back(order[i]);
             }
         }
-        return ans + no;
+        for(int i = 0 ; i < 26 ; i++) {
+            while(freq[i]-- > 0) {
+                ans.push_back(i + 'a');
+            }
+        }
+        return ans;
     }
 };
