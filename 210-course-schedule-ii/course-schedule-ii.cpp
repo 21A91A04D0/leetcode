ea7faf26738d6e9f963299d61221fedf80c6ef5c
siva_ganugula <98007068+21A91A04D0@users.noreply.github.com>
2024-03-11 11:09:25 +0530

    Time: 9 ms (95.49%) | Memory: 17.7 MB (37.99%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<int> ans;
+    void dfs_topo(int node, vector<int> adj[], vector<int> &visited) {
+        visited[node] = 1;
+        for(auto it : adj[node]) {
+            if(!visited[it]) {
+                dfs_topo(it, adj, visited);
+            }
+        }
+        ans.push_back(node);
+    }
+    bool dfsCheck(int node, vector<int> adj[], vector<int> &vis, vector<int> &pathVis) {
+        vis[node] = 1;
+        pathVis[node] = 1;
+        for(auto it : adj[node]) {
+            if(!vis[it]) {
+                if(dfsCheck(it, adj, vis, pathVis)) return true;
+            }
+            else if(pathVis[it]) return true;
+        }
+        pathVis[node] = 0;
+        return false;
+    }
+    vector<int> findOrder(int V, vector<vector<int>>& prerequisites) {
+        vector<int> adj[V];
+        for(auto it : prerequisites) {
+            adj[it[0]].push_back(it[1]);
+        }
+        vector<int> vis(V, 0);
+        vector<int> pathVis(V, 0);
+        vector<int> visited(V, 0);
+        //if cycle is detected then there is not topo sort
+        bool flag = true;
+        for(int i = 0 ; i < V ; i++) {
+            if(!vis[i] and dfsCheck(i, adj, vis, pathVis)) {
+                flag = false;
+                break;
+            }
+        }
+        
+        if(flag == false) return ans;
+        else {
+            for(int i = 0 ; i < V ; i++) {
+                if(!visited[i]) {
+                    dfs_topo(i, adj, visited);
+                }
+            }
+            // reverse(ans.begin(), ans.end());
+            return ans;
+        }
+    }
+};
