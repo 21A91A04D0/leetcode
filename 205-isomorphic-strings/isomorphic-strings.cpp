daae1c67495abc074c7496e942731f465e6e11ba
ganugula_siva <98007068+21A91A04D0@users.noreply.github.com>
2024-04-02 10:29:42 +0530

    Time: 12 ms (12.32%) | Memory: 9.4 MB (17.52%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char, int>ms;
+        map<char, int>mt;
+        for(auto it : s) {
+            ms[it]++;
+        }
+        for(auto it : t) {
+            mt[it]++;
+        }
+        map<char, char> mp;
+        if(s == t) return 1;
+        if(s.size() != t.size() || mt.size() != ms.size()) return false;
+        for(int i = 0 ; i < s.size() ; i++) {
+            
+            if(ms[s[i]] != mt[t[i]]) return 0;
+            else {
+                if(mp.find(s[i]) == mp.end()) {
+                    mp[s[i]] = t[i];
+                }
+                else {
+                    char prev = mp[s[i]];
+                    if(prev != t[i]) return 0;
+                }
+            }
+        }
+        return 1;
+    }
+};
