d90f1949e4ccdb5cdafa455d3845871af908326b
ganugula_siva <98007068+21A91A04D0@users.noreply.github.com>
2024-04-08 10:10:22 +0530

    Time: 14 ms (21.47%) | Memory: 14.3 MB (19.42%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,45 +1,57 @@
 class Solution {
 public:
-    bool safe(int row, int col, vector<string> &board, int n) {
-        int duprow = row;
-        int dupcol = col;
-        while(row >= 0 && col >= 0) {
-            if(board[row][col] == 'Q') return false;
-            row--;
-            col--;
-        }
-        row = duprow;
-        col = dupcol;
-        while(col >= 0) {
-            if(board[row][col] == 'Q') return false;
-            col--;
-        }
-        col = dupcol;
-        while(row < n && col >= 0) {
-            if(board[row][col] == 'Q') return false;
-            col--;
-            row++;
-        }
-        return true;
-    }
-    void solve(int col, vector<string> &board, vector<vector<string>> &ans, int n) {
+    // bool safe(int row, int col, vector<string> &board, int n) {
+    //     int duprow = row;
+    //     int dupcol = col;
+    //     while(row >= 0 && col >= 0) {
+    //         if(board[row][col] == 'Q') return false;
+    //         row--;
+    //         col--;
+    //     }
+    //     row = duprow;
+    //     col = dupcol;
+    //     while(col >= 0) {
+    //         if(board[row][col] == 'Q') return false;
+    //         col--;
+    //     }
+    //     col = dupcol;
+    //     while(row < n && col >= 0) {
+    //         if(board[row][col] == 'Q') return false;
+    //         col--;
+    //         row++;
+    //     }
+    //     return true;
+    // }
+    void solve(int col, vector<string> &board, vector<vector<string>> &ans, int n, vector<int> upperDiagonal, vector<int> lowerDiagonal, vector<int> leftSide) {
         if(col == n) {
             ans.push_back(board);
             return;
         }
         for(int row = 0 ; row < n ; row++) {
-            if(safe(row, col, board, n)) {
+            if(upperDiagonal[n - 1 + col - row] != 1 && lowerDiagonal[row + col] != 1 && leftSide[row] != 1) {
                 board[row][col] = 'Q';
-                solve(col + 1, board, ans, n);
+                lowerDiagonal[row + col] = 1;
+                leftSide[row] = 1;
+                upperDiagonal[n - 1 + col - row] = 1;
+                solve(col + 1, board, ans, n, upperDiagonal, lowerDiagonal, leftSide);
                 board[row][col] = '.';
+                lowerDiagonal[row + col] = 0;
+                leftSide[row] = 0;
+                upperDiagonal[n - 1 + col - row] = 0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
+        vector<int> upperDiagonal((2 * n) - 1, 0);
+        vector<int> lowerDiagonal((2 * n) - 1, 0);
+        vector<int> leftSide(n, 0);
         string s(n, '.');
         vector<vector<string>> ans;
         vector<string> board(n, s);
-        solve(0, board, ans, n);
+        
+        solve(0, board, ans, n, upperDiagonal, lowerDiagonal, leftSide);
+        
+        
         return ans;
 
     }
