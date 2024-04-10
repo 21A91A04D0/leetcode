d69206e129af171cd451da0e8a839bf798f080a6
ganugula_siva <98007068+21A91A04D0@users.noreply.github.com>
2024-04-10 21:16:03 +0530

    Time: 2 ms (37.45%) | Memory: 7.2 MB (35.91%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        // n = abs(n);
-        if(n != 0 and n != INT_MIN and (n & n - 1) == 0) return true;
+        if(n > 0 and (n & n - 1) == 0) return true;
         return false;
     }
 };
