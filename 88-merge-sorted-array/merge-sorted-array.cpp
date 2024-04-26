f0bf7b260261628bf42d8684590b6fe95cbacc00
ganugula_siva <98007068+21A91A04D0@users.noreply.github.com>
2024-04-26 14:29:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (71.84%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
+    void swapg(vector<int> &nums1, vector<int> &nums2, int ind1, int ind2) {
+        if(nums1[ind1] > nums2[ind2]) swap(nums1[ind1], nums2[ind2]);
+    }
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0;
-        int j = 0;
-        vector<int> ans;
-        while(i < m && j < n) {
-            if(nums1[i] <= nums2[j]) {
-                ans.push_back(nums1[i]);
-                i++;
+        //shell sort
+        int len = m + n;
+        int gap = (len / 2) + (len % 2);
+        while(gap > 0) {
+            int left = 0;
+            int right = left + gap;
+            while(right < len) {
+                if(left < m and right >= m) {
+                    swapg(nums1, nums2, left, right - m);
+                }
+                else if(left >= m) {
+                    swapg(nums2, nums2, left - m, right - m);
+                }
+                else {
+                    swapg(nums1, nums1, left, right);
+                }
+                left++;
+                right++;
             }
-            else {
-                ans.push_back(nums2[j]);
-                j++;  
-            }
+            if(gap == 1) break;
+            gap = (gap / 2) + (gap % 2);
         }
-        while(i < m) {
-            ans.push_back(nums1[i]);
-            i++;
-        }
-        while(j < n) {
-            ans.push_back(nums2[j]);
-            j++;
-        }
-        
-        for(int i = 0; i < nums1.size(); i++) {
-            nums1[i] = ans[i];
+        int k = 0;
+        for(int i = 0; i < n; i++) {
+            nums1[i + m] = nums2[k];
+            k++;
         }
     }
 };
