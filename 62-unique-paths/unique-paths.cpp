d5e09cb6692400642216155a307bbae861ed7bf6
ganugula_siva <98007068+21A91A04D0@users.noreply.github.com>
2024-04-27 09:38:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (86.23%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int dp[101][101];
-    int paths(int i, int j, int m, int n) {
-        
-        if(i == m - 1 and j == n - 1) return 1;
-        if(i >= m or j >= n) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = paths(i + 1, j, m, n) + paths(i, j + 1, m, n);
-
+    int fun(int row, int col, int m, int n) {
+        if(row == m and col == n) return 1;
+        if(row > m or col > n) return 0;
+        if(dp[row][col] != -1) return dp[row][col];
+        return dp[row][col] = fun(row + 1, col, m, n) + fun(row, col + 1, m, n);
     }
     int uniquePaths(int m, int n) {
         memset(dp, -1, sizeof(dp));
-        return paths(0, 0, m, n);
+        return fun(1, 1, m, n);
     }
 };
