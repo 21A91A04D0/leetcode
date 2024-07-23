52add26ee5fe1198ae71f2667b0eb56171bb5262
ganugula_siva <98007068+21A91A04D0@users.noreply.github.com>
2024-07-23 15:28:14 +0530

    Time: 4 ms (49.88%) | Memory: 17.3 MB (48.68%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -13,10 +13,9 @@ class Solution {
 public:
     bool fun(TreeNode *root1, TreeNode *root2) {
         if(!root1 and !root2) return true;
-        if(!root1 and root2 || root1 and !root2 || root1->val != root2->val) return false;
+        if(!root1 || !root2 || root1->val != root2->val) return false;
         int l = fun(root1->left, root2->right);
         int r = fun(root1->right, root2->left);
-        if(l == false || r == false) return false;
         return l && r;
     }
     bool isSymmetric(TreeNode* root) {
