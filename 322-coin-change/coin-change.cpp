5d7e37a6b96b5b3e9d51ffbeee165ee00939ea3f
ganugula_siva <98007068+21A91A04D0@users.noreply.github.com>
2024-09-20 17:24:22 +0530

    Time: 64 ms (12.20%) | Memory: 80.7 MB (5.04%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    long long dp[13][10001];
-    long long coins(vector<int> &nums, int target, int ind) {
-        if (ind >= nums.size()) {
-            if(target == 0) return 0;
+
+    long long helper(int target,int ind,  vector<int> &coins, vector<vector<long long>> &dp) {
+        if(target == 0) {
+            return 0;
+        }
+        if(ind >= coins.size() || target < 0) {
             return INT_MAX;
         }
         if(dp[ind][target] != -1) return dp[ind][target];
-        // pick
-        long long a = INT_MAX;
-        long long b = INT_MAX;
-        if(nums[ind] <= target) {
-            a = 1 + coins(nums, target - nums[ind], ind);
-        }
-        // non - pick
-        b = coins(nums, target, ind + 1);
-        return dp[ind][target] = min(a, b);
+        long long pick = 1 + helper(target - coins[ind], ind, coins, dp);
+        if(pick >= INT_MAX) pick = INT_MAX;
+        long long notpick = helper(target , ind + 1 , coins, dp);
+        return dp[ind][target] = min(pick, notpick);
     }
-    int coinChange(vector<int>& coin, int amount) {
-        memset(dp, -1, sizeof(dp));
-        int res = coins(coin, amount, 0);
-        if(res == INT_MAX) return -1;
-        return res;
+    int coinChange(vector<int>& coins, int amount) {
+        vector<vector<long long>> dp(coins.size(), vector<long long>(amount + 1, -1));
+
+        long long ans = helper(amount, 0, coins, dp);
+        if(ans >= INT_MAX) return -1;
+        else return (int)ans;
     }
 };
