b46b3d612ce39d51b76bead8379631fa415409e6
ganugula_siva <98007068+21A91A04D0@users.noreply.github.com>
2024-09-22 16:29:22 +0530

    Time: 29 ms (17.63%) | Memory: 16.6 MB (29.53%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        // return "";
-        vector<int> zig;
-        int k = 0;
-        int i = 0;
-        if(numRows < 2) return s;
-        while(i < s.size()) {
-            while(k < numRows && i < s.size()) {
-                zig.push_back(k);
-                k++;
-                i++;
-            }
-            k-=2;
-            while(k >= 0 && i < s.size()) {
-                zig.push_back(k);
-                k--;
-                i++;
-            }
-            k+=2;
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
         }
-        string res = "";
-        for(int i = 0; i < numRows; i++) {
-            for(int j = 0; j < s.size(); j++) {
-                if(i == zig[j]) res.push_back(s[j]);
+
+        int idx = 0, d = 1;
+        vector<vector<char>> rows(numRows);
+
+        for (char c : s) {
+            rows[idx].push_back(c);
+            if (idx == 0) {
+                d = 1;
+            } else if (idx == numRows - 1) {
+                d = -1;
+            }
+            idx += d;
+        }
+
+        string result;
+        for (const auto& row : rows) {
+            for (char c : row) {
+                result += c;
             }
         }
-        return res;
+
+        return result;        
     }
 };
